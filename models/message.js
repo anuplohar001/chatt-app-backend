30de31a05f2266f8735dcbd6c4067e09c9776fc6
anuplohar001 <anuplohar001@gmail.com>
2025-01-23 17:59:49 +0530

    first commit

---
+++ models/message.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    from: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+        require: true
+    },
+
+    to: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+    },
+
+    toGroup: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'groups'
+    },
+
+    content: {
+        type: String,
+        require: true
+    },
+
+    date: {
+        type: String,
+        require: true
+    }
+})
+
+const Messages = mongoose.model.messages || mongoose.model("messages", messageSchema)
+export default Messages
---
+++ models/user.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: [true, 'Username is required !'],
+    },
+    password: {
+        type: String,
+    },
+    liveUser: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users'
+    }]
+});
+
+const User = mongoose.models.user || mongoose.model("Users", userSchema);
+export default User
